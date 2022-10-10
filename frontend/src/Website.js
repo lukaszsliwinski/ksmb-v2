6715170325f0ca29b6bfac947fa89589d303beb3
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-10-10 21:38:04 +0200

    Create preliminary layout with parallax effect

---
+++ frontend/src/Website.js
@@ -0,0 +1,35 @@
+import 'tw-elements';
+import './Website.css';
+
+function Website() {
+    return (
+        <>
+            <header className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover bg-photo-1">
+
+            </header>
+            <div className="max-w-lg m-auto h-full min-h-screen">
+                <p>o nas</p>
+            </div>
+            <section className="flex items-center justify-center h-2screen/3 m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-2">
+
+            </section>
+            <div className="max-w-lg m-auto h-full min-h-screen">
+                <p>nagrania</p>
+            </div>
+            <section className="flex items-center justify-center h-2screen/3 m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-3">
+
+            </section>
+            <div className="max-w-lg m-auto h-full min-h-screen">
+                <p>koncerty</p>
+            </div>
+            <section className="flex items-center justify-center h-2screen/3 m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-4">
+
+            </section>
+            <div className="max-w-lg m-auto h-screen/3">
+                <p>kontakt</p>
+            </div>
+        </>
+    );
+}
+
+export default Website;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
     theme: {
-        extend: {},
+        minHeight: {
+            'screen/2': '50vh',
+            'screen': '100vh',
+        },
+        extend: {
+            height: {
+                'screen/3': '33.33vh',
+                'screen/2': '50vh',
+                '2screen/3': '66.66vh',
+            },
+        },
     },
     plugins: [
         require('tw-elements/dist/plugin')
