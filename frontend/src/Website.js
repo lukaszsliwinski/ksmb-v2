2489aeb3289174c5b3f448fae9236acc5130042c
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-10-17 20:50:42 +0200

    Add svg logo to landing page

--- frontend/src/Website.js
+++ frontend/src/Website.js
@@ -1,31 +1,46 @@
 import 'tw-elements';
+import logo from './logo.svg';
 import './Website.css';
 
 function Website() {
     return (
         <>
             <header className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover bg-photo-1">
+                <div className="w-screen h-screen bg-black/40 backdrop-blur-sm p-20">
+                    <div className="flex items-center justify-center border-4 border-white/50 h-full">
+                        <img src={logo} className="h-[16rem]" />
+                        {/* <h1 className="text-[10rem] text-neutral-50"><b>KSM</b>icroband</h1> */}
+                    </div>
+
+                </div>
+
+{/* filtr do zdjęcia głównego */}
+{/* saturate-[.75] grayscale-[30%] */}
+
+{/* filtr do zdjęć*/}
+{/* before:content-[''] before:absolute before:inset-0 before:bg-black before:opacity-50" */}
+                
 
             </header>
             <div className="max-w-lg m-auto h-full min-h-screen">
                 <p>o nas</p>
             </div>
-            <section className="flex items-center justify-center h-2screen/3 m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-2">
+            <section className="flex items-center justify-center h-[66vh] m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-2">
 
             </section>
             <div className="max-w-lg m-auto h-full min-h-screen">
                 <p>nagrania</p>
             </div>
-            <section className="flex items-center justify-center h-2screen/3 m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-3">
+            <section className="flex items-center justify-center h-[66vh] m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-3">
 
             </section>
             <div className="max-w-lg m-auto h-full min-h-screen">
                 <p>koncerty</p>
             </div>
-            <section className="flex items-center justify-center h-2screen/3 m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-4">
+            <section className="flex items-center justify-center h-[75vh] m-auto mb-12 bg-fixed bg-center bg-cover bg-photo-4">
 
             </section>
-            <div className="max-w-lg m-auto h-screen/3">
+            <div className="max-w-lg m-auto h-[25vh]">
                 <p>kontakt</p>
             </div>
         </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,17 +2,7 @@
 module.exports = {
     content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
     theme: {
-        minHeight: {
-            'screen/2': '50vh',
-            'screen': '100vh',
-        },
-        extend: {
-            height: {
-                'screen/3': '33.33vh',
-                'screen/2': '50vh',
-                '2screen/3': '66.66vh',
-            },
-        },
+        extend: {},
     },
     plugins: [
         require('tw-elements/dist/plugin')
