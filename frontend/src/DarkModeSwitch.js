25794bad7b62aeae81fa5a452951c0a32f16228f
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-10-26 17:51:22 +0200

    correct dark mode switch style

--- frontend/src/DarkModeSwitch.js
+++ frontend/src/DarkModeSwitch.js
@@ -5,8 +5,8 @@ function DarkModeSwitch({ darkMode, setDarkMode }) {
     return (
         <div onClick={() => setDarkMode(!darkMode)}>
             <div className={`
-                w-[3.75rem] h-8 bg-temp-dark dark:bg-temp-light dark:border-0 rounded-[16px] p-1 relative cursor-pointer
-                before:content-[''] before:block before:h-6 before:w-6 before:rounded-[12px]
+                w-[3.75rem] h-8 bg-transparent dark:bg-temp-light border dark:border-0 rounded-[16px] p-1 relative cursor-pointer
+                before:content-[''] before:block before:h-[1.375rem] dark:before:h-6 before:w-[1.375rem] dark:before:w-6 before:rounded-[12px]
                 before:bg-temp-light before:dark:bg-temp-dark before:absolute before:z-[2] before:translate-x-0
                 before:transition-transform before:duration-100 before:ease-linear
                 ${darkMode ? 'before:translate-x-7' : ''}
