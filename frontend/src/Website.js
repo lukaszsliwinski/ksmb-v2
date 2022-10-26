16233320f8a0daeb4cd9d02795ec3c6acd7e217c
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-10-26 17:31:36 +0200

    add links to nav items

--- frontend/src/Website.js
+++ frontend/src/Website.js
@@ -9,9 +9,15 @@ function Website() {
     const [darkMode, setDarkMode] = useState(false);
     const [windowTop, setWindowTop] = useState(true);
 
+    const html = document.documentElement;
+    
     useEffect(() => {
-        darkMode ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
-    }, [darkMode])
+        html.classList.add('scroll-smooth');
+    }, []);
+
+    useEffect(() => {
+        darkMode ? html.classList.add('dark') : html.classList.remove('dark');
+    }, [darkMode]);
 
     useEffect(() => {
         const handleScroll = () => window.scrollY === 0 ? setWindowTop(true) : setWindowTop(false);
@@ -23,24 +29,24 @@ function Website() {
 
 
     return (
-        <div className="all-inside:transition-colors all-inside:ease-in-out all-inside:duration-200">
+        <div className="all-inside:transition-colors all-inside:ease-in-out all-inside:duration-200" id="main">
             <nav className={`fixed z-10 flex w-full h-16 px-5 ${windowTop ? 'bg-transparent' : 'bg-temp-dark/80 backdrop-blur-md'} ease-in-out duration-300`}>
                 <div className="basis-1/3 flex items-center text-[1.8rem] text-temp-light">
-                    <span>KSMicroband</span>
+                    <a className="nav-link text-temp-light" href="#main">KSMicroband</a>
                 </div>
                 <div className="basis-1/3 flex justify-center items-center h-full">
                     <ul className="flex flex-row space-x-6 list-style-none">
                         <li className="text-[1.8rem]">
-                            <a className="nav-link text-temp-light" href="#">o nas</a>
+                            <a className="nav-link text-temp-light" href="#about">o nas</a>
                         </li>
                         <li className="text-[1.8rem]">
-                            <a className="nav-link text-temp-light" href="#">muzyka</a>
+                            <a className="nav-link text-temp-light" href="#music">muzyka</a>
                         </li>
                         <li className="text-[1.8rem]">
-                            <a className="nav-link text-temp-light" href="#">koncerty</a>
+                            <a className="nav-link text-temp-light" href="#concerts">koncerty</a>
                         </li>
                         <li className="text-[1.8rem]">
-                            <a className="nav-link text-temp-light" href="#">kontakt</a>
+                            <a className="nav-link text-temp-light" href="#footer">kontakt</a>
                         </li>
                     </ul>
                 </div>
@@ -58,25 +64,25 @@ function Website() {
                     </div>
                 </div>
             </section>
-            <div className="w-full h-full min-h-screen bg-temp-light dark:bg-temp-dark">
+            <div className="w-full h-full min-h-screen bg-temp-light dark:bg-temp-dark" id="about">
                 <p className="text-temp-dark dark:text-temp-light">o nas</p>
             </div>
             <section className="flex items-center justify-center h-[66vh] m-auto bg-fixed bg-center bg-photo-2 bg-100%">
                 <div className="w-screen h-full bg-black/60"></div>
             </section>
-            <div className="w-full h-full min-h-screen bg-temp-light dark:bg-temp-dark">
-                <p className="text-temp-dark dark:text-temp-light">nagrania</p>
+            <div className="w-full h-full min-h-screen bg-temp-light dark:bg-temp-dark" id="music">
+                <p className="text-temp-dark dark:text-temp-light">muzyka</p>
             </div>
             <section className="flex items-center justify-center h-[66vh] m-auto bg-fixed bg-center bg-photo-3 bg-100%">
                 <div className="w-screen h-full bg-black/60"></div>
             </section>
-            <div className="w-full h-full min-h-screen bg-temp-light dark:bg-temp-dark">
+            <div className="w-full h-full min-h-screen bg-temp-light dark:bg-temp-dark" id="concerts">
                 <p className="text-temp-dark dark:text-temp-light">koncerty</p>
             </div>
-            <section className="flex items-center justify-center h-[65vh] m-auto bg-fixed bg-center bg-photo-4 bg-100%">
+            <section className="flex items-center justify-center h-[66vh] m-auto bg-fixed bg-center bg-photo-4 bg-100%">
                 <div className="w-screen h-full bg-black/60"></div>
             </section>
-            <div className="w-full h-[35vh] bg-temp-light dark:bg-temp-dark">
+            <div className="w-full h-[34vh] bg-temp-light dark:bg-temp-dark" id="footer">
                 <p className="text-temp-dark dark:text-temp-light">kontakt</p>
             </div>
         </div>
