7637386530f1c90c6a1ef6431ec9764466dab0d6
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-10-28 14:54:34 +0200

    Create cards with bio

--- frontend/src/Website.js
+++ frontend/src/Website.js
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react';
 import 'tw-elements';
 
 import { ReactComponent as Logo } from './logo.svg';
+import kubaBio from './photos/kubaBio.jpg';
+import lukaszBio from './photos/lukaszBio.jpg';
+import microbandBio from './photos/microbandBio.jpg';
+
 import DarkModeSwitch from './DarkModeSwitch';
 import './Website.css';
 
@@ -64,8 +68,81 @@ function Website() {
                     </div>
                 </div>
             </section>
-            <div className="w-full h-full min-h-screen bg-temp-light dark:bg-temp-dark" id="about">
-                <p className="text-temp-dark dark:text-temp-light">o nas</p>
+
+
+            <div className="w-full h-full min-h-screen p-28 bg-temp-light dark:bg-temp-dark" id="about">
+
+                    <div className="block mb-10 rounded-lg shadow-lg max-w-6xl bg-white">
+                        <div className="flex flex-wrap items-center">
+                            <div className="grow-0 shrink-0 basis-auto block lg:flex w-full lg:w-6/12 xl:w-4/12">
+                                <img src={kubaBio} alt="img" className="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg slopedframe" />
+                            </div>
+                            <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+                                <div className="px-3 py-6 md:px-6">
+                                    <h2 className="text-3xl font-bold mb-2 ">Kuba Kołodziejczyk</h2>
+                                    <p className="font-semibold mb-4">skrzypce</p>
+                                    <p className="text-justify text-gray-500 mb-6">
+                                        Skrzypce towarzyszą mi od 7&nbsp;roku życia, 
+                                        obecnie gram muzykę klasyczną i&nbsp;rozrywkową ale też uczę jak na nich grać. 
+                                        Zagrałem około 500 koncertów na scenach Śląska i&nbsp;w&nbsp;innych rejonach kraju, 
+                                        a&nbsp;także w&nbsp;Niemczech, Austrii, Włoszech, Hiszpanii oraz Japonii. Granie muzyki jest 
+                                        dla mnie taką fajną podróżą, gdzie ciągle można odkrywać nowe horyzonty i&nbsp;barwy. 
+                                        Takim właśnie muzycznym podróżowaniem jest granie z&nbsp;Łukaszem.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="block ml-auto mb-10 rounded-lg shadow-lg max-w-6xl bg-white">
+                        <div className="flex flex-wrap items-center">
+                            <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+                                <div className="px-3 py-6 md:px-6">
+                                    <h2 className="text-3xl font-bold mb-2 ">Łukasz Śliwiński</h2>
+                                    <p className="font-semibold mb-4">gitara</p>
+                                    <p className="text-justify text-gray-500 mb-6">
+                                        Na gitarze gram od 15&nbsp;roku życia zdobywając doświadczenie sceniczne i&nbsp;studyjne 
+                                        w&nbsp;zespołach rockowych. Od kilku lat rozszerzam spektrum zainteresowań sięgając po 
+                                        gatunki muzyczne o&nbsp;różnej estetyce, kierując swoją uwagę głównie w&nbsp;stronę muzyki 
+                                        improwizowanej. Stawiam na przestrzeń, spontaniczność i&nbsp;nieprzewidywalność -&nbsp;najwięcej 
+                                        radości w&nbsp;grze sprawia mi, kiedy nie wiem jaki będzie następny dźwięk. 
+                                        Potrzebę muzycznej wolności zaspokajam grając w Microbandzie.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="grow-0 shrink-0 basis-auto block lg:flex w-full lg:w-6/12 xl:w-4/12">
+                                <img src={lukaszBio} alt="img" className="w-full rounded-t-lg lg:rounded-tl-none lg:rounded-br-lg slopedframe2" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="block mb-10 rounded-lg shadow-lg max-w-6xl bg-white">
+                        <div className="flex flex-wrap items-center">
+                            <div className="grow-0 shrink-0 basis-auto block lg:flex w-full lg:w-6/12 xl:w-4/12">
+                                <img src={microbandBio} alt="img" className="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg slopedframe" />
+                            </div>
+                            <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+                                <div className="px-3 py-6 md:px-6">
+                                    <h2 className="text-3xl font-bold mb-2 ">Microband</h2>
+                                    <p className="text-justify text-gray-500 mb-6">
+                                        Pomysł na duet narodził się na wspólnych spotkaniach, których jedynym celem było oderwanie się 
+                                        od dotychczasowych przyzwyczajeń muzycznych, rockowych oraz klasycznych. Z&nbsp;połączenia różnych, 
+                                        pozornie odmiennych temperamentów muzycznych, uzyskaliśmy nową jakość, cały czas ucząc się od siebie 
+                                        innego spojrzenia na muzykę. Aby nie zamykać sobie na starcie żadnej drogi postanowiliśmy nie 
+                                        umawiać się na konkretną stylistykę, tylko zobaczyć, jaka będzie dla nas najbardziej naturalna. 
+                                        Inspiracje przychodziły z&nbsp;takich stron, jak muzyka folkowa, rockowa, jazzowa, klasyczna, filmowa, 
+                                        bałkańska, a&nbsp;nawet hebrajska. Z&nbsp;improwizowanych sesji powoli zbieraliśmy materiał, który 
+                                        uporządkowaliśmy w&nbsp;autorskie kompozycje instrumentalne. Wykorzystując czas pandemii i&nbsp;niemożność 
+                                        koncertowania zarejestrowaliśmy w domach naszą twórczość. Czujemy, że nadszedł czas na kolejny krok 
+                                        -&nbsp;przedstawienie się, pokazanie szerzej i&nbsp;granie dla ludzi. Rozpoczynamy wspólną podróż, 
+                                        nie wiedząc gdzie nas zaprowadzi. I&nbsp;to w&nbsp;tym wszystkim lubimy najbardziej.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
             </div>
             <section className="flex items-center justify-center h-[66vh] m-auto bg-fixed bg-center bg-photo-2 bg-100%">
                 <div className="w-screen h-full bg-black/60"></div>
