2777a8b5e756d3e1d11442bd7cf48a4dccc4a8c5
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-11-28 21:41:48 +0100

    style: prepare music section for mobile

--- frontend/src/layouts/Music.js
+++ frontend/src/layouts/Music.js
@@ -4,16 +4,16 @@ import VideoPlayer from '../components/VideoPlayer';
 function Music() {
   return (
     <section
-      className="flex h-full min-h-screen w-full bg-white p-28 dark:bg-outer-space"
+      className="flex h-full min-h-screen w-full flex-col bg-white py-28 px-6 dark:bg-outer-space md:flex-row xl:px-28"
       id="music"
     >
-      <div className="flex w-1/2 justify-center">
-        <div className="w-9/12 max-w-6xl">
+      <div className="flex justify-center md:w-1/2">
+        <div className="w-11/12 max-w-6xl xl:w-9/12">
           <AudioPlayer />
         </div>
       </div>
-      <div className="flex w-1/2 justify-center">
-        <div className="w-9/12 max-w-6xl">
+      <div className="flex justify-center md:w-1/2">
+        <div className="w-11/12 max-w-6xl xl:w-9/12">
           <VideoPlayer />
         </div>
       </div>
