318b0dfe056f5bf17a189c8f10dd568ad8b2b6ed
lukaszliwinski <lukaszsliwinski0@gmail.com>
2022-11-30 22:51:42 +0100

    fix: correct pdf download href

--- frontend/src/components/DownloadBtn.js
+++ frontend/src/components/DownloadBtn.js
@@ -1,7 +1,7 @@
 function DownloadBtn({ label, file, download }) {
   return (
     <a
-      href={`hostname/api/pdf/${file}`}
+      href={`/api/pdf/${file}`}
       download={download}
       className="my-1 inline-block rounded border-2 border-outer-space px-6 py-2 text-center text-xs font-medium uppercase leading-tight text-outer-space transition-colors focus:outline-none focus:ring-0 hover:border-supernova hover:text-supernova dark:border-white dark:text-white dark:hover:border-supernova dark:hover:text-supernova"
     >
