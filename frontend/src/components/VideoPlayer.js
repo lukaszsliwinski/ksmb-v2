1ea77f2a74f16f84770e475bf71507ef1ba26ff8
lukaszliwinski <lukaszsliwinski0@gmail.com>
2022-12-01 21:23:41 +0100

    fix: back to <iframe> in video player

--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
 function VideoPlayer() {
-    // videos list with urls
-    const videos = [
-      ['Hague (Home Sessions #1)', 'TPnMZWR_2To'],
-      ['Sangria (In The Nature #2)', '4IErWZv3MCE'],
-      ['Swinger Dance (In The Nature #1)', '_xKkcRnJPlc']
-    ];
+  // videos list with urls
+  const videos = [
+    ['Hague (Home Sessions #1)', 'TPnMZWR_2To'],
+    ['Sangria (In The Nature #2)', '4IErWZv3MCE'],
+    ['Swinger Dance (In The Nature #1)', '_xKkcRnJPlc']
+  ];
 
   // state
   const [url, setUrl] = useState(videos[0][1]);
@@ -16,11 +16,13 @@ function VideoPlayer() {
       <div className="mx-auto rounded-lg bg-silver/20 text-outer-space shadow-lg dark:text-white">
         <div className="p-4">
           <div className="embed-responsive embed-responsive16by9 relative w-full overflow-hidden rounded-lg pt-[56.25%]">
-            <object
+            <iframe
               title="KSMicroband Youtube Video Player"
               className="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 h-full w-full"
-              data={`https://www.youtube.com/embed/${url}`}
-            ></object>
+              src={`https://www.youtube.com/embed/${url}?enablejsapi=1&amp;origin=https%3A%2F%2Fksmicroband.pl`}
+              allowFullScreen="1"
+              data-gtm-yt-inspected-2340190_699="true"
+            ></iframe>
           </div>
         </div>
 
